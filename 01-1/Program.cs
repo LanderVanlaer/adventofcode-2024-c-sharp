75f2e4cea85e6432f3482cb15a92f39ef6777561
Lander Van laer <lander.vanlaer1@gmail.com>
2024-12-01 13:52:53 +0100

    Day 1 - Round 1

---
+++ 01-1/Program.cs
@@ -0,0 +1,18 @@
+﻿StreamReader file = new(args[0]);
+
+var comparer = Comparer<int>.Create((a, b) => a == b ? -1 : a - b);
+
+SortedSet<int> leftList = new(comparer);
+SortedSet<int> rightList = new(comparer);
+
+while (await file.ReadLineAsync() is { } line)
+{
+    leftList.Add(int.Parse(line[..5]));
+    rightList.Add(int.Parse(line[8..]));
+}
+
+file.Close();
+
+var total = leftList.Zip(rightList).Aggregate(0, (acc, pair) => acc + Math.Abs(pair.First - pair.Second));
+
+Console.WriteLine(total);
