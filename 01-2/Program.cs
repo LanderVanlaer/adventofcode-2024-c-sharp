de74225de9857c9589597dd6c732a0d4c9009b27
Lander Van laer <lander.vanlaer1@gmail.com>
2024-12-01 14:56:44 +0100

    Day 1 - Round 2

---
+++ 01-2/Program.cs
@@ -0,0 +1,19 @@
+﻿StreamReader file = new(args[0]);
+
+List<int> leftList = [];
+Dictionary<int, short> rightMap = new();
+
+while (await file.ReadLineAsync() is { } line)
+{
+    var l = int.Parse(line[..5]);
+    var r = int.Parse(line[8..]);
+
+    leftList.Add(l);
+    rightMap[r] = (short)(rightMap.TryGetValue(r, out var count) ? count + 1 : 1);
+}
+
+file.Close();
+
+var total = leftList.Aggregate(0, (acc, l) => rightMap.TryGetValue(l, out var count) ? acc + l * count : acc);
+
+Console.WriteLine(total);
