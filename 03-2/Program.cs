23e54fb373451f65ce08311ba4b113052caff146
Lander Van laer <lander.vanlaer1@gmail.com>
2024-12-04 21:22:56 +0100

    Day 3 - Round 2

---
+++ 03-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+StreamReader file = new(args[0]);
+
+var total = 0;
+var regex = MyRegex();
+var dont = false;
+
+while (await file.ReadLineAsync() is { } line)
+{
+    var matches = regex.Matches(line);
+    if (matches.Count == 0) continue;
+
+    foreach (Match match in matches)
+        if (match.Groups[4].Success)
+        {
+            dont = true;
+        }
+        else if (match.Groups[3].Success)
+        {
+            dont = false;
+        }
+        else if (!dont)
+        {
+            var a = int.Parse(match.Groups[1].Value);
+            var b = int.Parse(match.Groups[2].Value);
+            total += a * b;
+        }
+}
+
+file.Close();
+
+Console.WriteLine(total);
+
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+internal partial class Program
+{
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))")]
+    private static partial Regex MyRegex();
+}
