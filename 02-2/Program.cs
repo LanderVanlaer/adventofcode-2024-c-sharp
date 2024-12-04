596ba1d834073d6985d677539d7d02dc937cd0b0
Lander Van laer <lander.vanlaer1@gmail.com>
2024-12-04 21:03:27 +0100

    DAy 2 - Round 2

---
+++ 02-2/Program.cs
@@ -0,0 +1,55 @@
+﻿StreamReader file = new(args[0]);
+
+var totalSafe = 0;
+
+while (await file.ReadLineAsync() is { } line)
+{
+    var arr = line.Split(' ').Select(int.Parse).ToArray();
+
+    for (var i = -1; i < arr.Length; i++)
+        if (IsSafe(EnumerateExcept(arr, i).GetEnumerator()))
+        {
+            ++totalSafe;
+            break;
+        }
+}
+
+file.Close();
+
+Console.WriteLine(totalSafe);
+return;
+
+bool IsSafe(IEnumerator<int> enumerator)
+{
+    enumerator.MoveNext();
+    var prev = enumerator.Current;
+
+    enumerator.MoveNext();
+    var current = enumerator.Current;
+
+    var downWards = current < prev;
+
+    if (current - prev is 0 or > 3 or < -3) return false;
+
+    while (enumerator.MoveNext())
+    {
+        prev = current;
+        current = enumerator.Current;
+
+        if (current - prev is 0 or > 3 or < -3
+            || (downWards && prev < current)
+            || (!downWards && prev > current))
+            return false;
+    }
+
+    return true;
+}
+
+IEnumerable<int> EnumerateExcept(int[] source, int index)
+{
+    for (var i = 0; i < source.Length; i++)
+    {
+        if (i == index) continue;
+        yield return source[i];
+    }
+}
