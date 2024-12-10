5fbd874e872e7c5070dd2f120581b7932e953827
Lander Van laer <lander.vanlaer1@gmail.com>
2024-12-10 23:42:11 +0100

    Day 9 - Round 1

---
+++ 09-1/Block.cs
@@ -0,0 +1,7 @@
+namespace _09_1;
+
+public struct Block
+{
+    public ushort Length { get; set; }
+    public short Id { get; set; }
+}
---
+++ 09-1/Program.cs
@@ -0,0 +1,73 @@
+﻿using _09_1;
+
+var list = new LinkedList<Block>();
+using (StreamReader file = new(args[0]))
+{
+    var line = await file.ReadLineAsync();
+    if (line is null) throw new Exception("Invalid input");
+
+    short i = 0;
+    foreach (var length in line.Select(c => c - '0'))
+    {
+        if (length is < 0 or > 9) throw new Exception("Invalid input");
+
+        if (length == 0)
+        {
+            ++i;
+            continue;
+        }
+
+        var isEmptySpace = i % 2 == 1;
+
+        list.AddLast(new Block { Length = (ushort)length, Id = (short)(isEmptySpace ? -1 : i / 2) });
+
+        i++;
+    }
+}
+
+var node = list.First;
+
+while (node is not null)
+{
+    if (node.ValueRef.Id >= 0)
+    {
+        node = node.Next;
+        if (node is null) Console.WriteLine("NULL");
+
+        continue;
+    }
+
+    var totalLength = node.ValueRef.Length;
+
+    while (list.Last!.ValueRef.Id < 0) list.RemoveLast();
+
+    var lastNode = list.Last!;
+
+    if (totalLength > lastNode.ValueRef.Length)
+    {
+        list.AddBefore(node, lastNode.ValueRef);
+        list.RemoveLast();
+        node.ValueRef.Length -= lastNode.ValueRef.Length;
+    }
+    else if (totalLength < lastNode.ValueRef.Length)
+    {
+        lastNode.ValueRef.Length -= totalLength;
+        node.ValueRef.Id = lastNode.ValueRef.Id;
+        node = node.Next;
+    }
+    else
+    {
+        node.ValueRef.Id = lastNode.ValueRef.Id;
+        list.RemoveLast();
+        node = node.Next;
+    }
+}
+
+var checksum = 0L;
+
+var count = 0;
+foreach (var block in list)
+    for (var i = 0; i < block.Length; i++)
+        checksum += (long)block.Id * count++;
+
+Console.WriteLine(checksum);
